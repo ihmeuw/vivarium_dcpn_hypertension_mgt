d8a04c9b81213622768f2f808319956f589a5093
Kate Wilson <kwilson7@uw.edu>
2019-07-23 15:14:58 -0700

    avoid reindexing warning pt 2

--- src/vivarium_dcpn_hypertension_mgt/tools/utilities.py
+++ src/vivarium_dcpn_hypertension_mgt/tools/utilities.py
@@ -124,7 +124,7 @@ def create_draw_level_data(data, measure, columns_to_keep):
 
     np.random.seed(RANDOM_SEED)
     d = np.random.random(1000)
-    for row in data[~no_ci_to_convert].iterrows():
+    for row in data.loc[~no_ci_to_convert].iterrows():
         dist = Normal(mean=row[1]['mean'], sd=row[1]['sd'])
         draws = dist.ppf(d)
         data.loc[row[0], DRAW_COLUMNS] = draws
