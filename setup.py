68f1d0e17a3d11aa741e95cd664a913d627dbda7
Kate Wilson <kwilson7@uw.edu>
2019-07-24 12:05:24 -0700

    missing comma

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
         'vivarium_public_health==0.9.16',
         'vivarium_cluster_tools==1.0.14',
         'vivarium_inputs[data]==3.0.1',
-        'risk_distributions==2.0.1'
+        'risk_distributions==2.0.1',
         
         # These are pinned for internal dependencies on IHME libraries
         'numpy<=1.15.4',
