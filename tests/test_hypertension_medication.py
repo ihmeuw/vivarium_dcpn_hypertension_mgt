024a558c6058e9f75aef5f37cab81b32a7cdd6ea
Kate Wilson <kwilson7@uw.edu>
2019-08-10 20:35:44 -0700

    add test for finding closest in efficacy

--- tests/test_hypertension_medication.py
+++ tests/test_hypertension_medication.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pytest
 
-from vivarium_dcpn_hypertension_mgt.components import hypertension_medication
+from vivarium_dcpn_hypertension_mgt.components import hypertension_medication, utilities
 
 
 def test_TreatmentProfile_validate():
@@ -33,3 +33,17 @@ def test_TreatmentProfile_add_transition():
 
     with pytest.raises(ValueError, match='over 1'):
         tx_profile.add_transition(None, probability_func=lambda index: pd.Series(0.35, index=index), domain_filter='a')
+
+
+@pytest.mark.parametrize('efficacy, result_positions', [(0.9, [1, 2]),
+                                                        (1.2, [2, 3]),
+                                                        (4.5, [5, 6]),
+                                                        (4.55, [6]),
+                                                        (5, [6]),
+                                                        (5.2, [])])
+def test_get_closest_in_efficacy_in_ramp(efficacy, result_positions):
+    efficacy_data = pd.Series([1, 2, 3, 4, 4.5, 5], index=[f'ramp_{n+1}' for n in range(6)])
+
+    closest_ramp_positions = utilities.get_closest_in_efficacy_in_ramp(efficacy, efficacy_data, 'ramp')
+
+    assert set([f'ramp_{n}' for n in result_positions]) == set(closest_ramp_positions.index)
