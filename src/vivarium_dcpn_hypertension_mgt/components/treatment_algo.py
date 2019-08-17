6f284daee793e007315955ba4d1fc1385a772892
kjells <kjells@uw.edu>
2019-08-16 18:36:25 -0700

    add measuredBP

--- src/vivarium_dcpn_hypertension_mgt/components/treatment_algo.py
+++ src/vivarium_dcpn_hypertension_mgt/components/treatment_algo.py
@@ -1,6 +1,9 @@
 # from loguru import logger
 import pandas as pd
+import numpy as np
+import scipy
 from vivarium.framework.engine import Builder
+from vivarium.framework.engine import Event
 
 class Adherence:
     @property
@@ -40,4 +43,66 @@ class Adherence:
 
     def get_appt_followup(self, index) -> pd.Series:
         return self.rand_appt_followup.get_draw(index) < self.df_ad_data.appt_ad
+
+
+class MeasuredSBP:
+    @property
+    def name(self):
+        return "high_systolic_blood_pressure"
+
+    configuration_defaults = {
+        'measurement': {
+            'error_sd': 6,
+        }
+    }
+
+    def __init__(self):
+        self.configuration_defaults = {
+            f'{self.name}_measurement': MeasuredSBP.configuration_defaults['measurement']
+        }
+
+    def setup(self, builder: Builder):
+        self.measurement_error = builder.configuration[f'{self.name}_measurement'].error_sd
+        self.randomness = builder.randomness.get_stream(f'{self.name}.measurement')
+        self.true_exposure = builder.value.get_value(f'{self.name}.exposure')
+
+        self.measurement_column = f'{self.name}_measurement'
+        columns_created = [self.measurement_column]
+
+        builder.population.initializes_simulants(self.on_initialize_simulants,
+                                                 creates_columns=columns_created)
+
+        self.population_view = builder.population.get_view(columns_created)
+
+        builder.event.register_listener('time_step__prepare', self.on_time_step_prepare)
+
+    def on_initialize_simulants(self, pop_data):
+        self.population_view.update(
+            pd.Series(np.nan, name=self.measurement_column, index=pop_data.index)
+        )
+
+    def on_time_step_prepare(self, event: Event):
+        self.population_view.update(pd.Series(np.nan, index=event.index, name=self.measurement_column))
+
+    def __call__(self, idx_measure: pd.Index, idx_record_these: pd.Index, measure_type_average: bool = False):
+        draw = self.randomness.get_draw(idx_measure)
+        if self.measurement_error:
+            noise = scipy.stats.norm.ppf(draw, scale=self.measurement_error)
+        else:
+            noise = 0
+
+        true_exp = self.true_exposure(idx_measure)
+        detect_zero = true_exp[true_exp==0]
+        hypertension_measurement = self.true_exposure(idx_measure) + noise
+        hypertension_measurement.loc[detect_zero.index] = 0.0
+
+        if measure_type_average:
+            hypertension_measurement = (hypertension_measurement +
+                                        self.population_view.get(idx_measure)[self.measurement_column]) / 2
+
+        measurement = self.population_view.get(idx_measure)[self.measurement_column]
+        measurement.loc[idx_record_these] = hypertension_measurement
+        self.population_view.update(measurement)
+
+        return hypertension_measurement
 
