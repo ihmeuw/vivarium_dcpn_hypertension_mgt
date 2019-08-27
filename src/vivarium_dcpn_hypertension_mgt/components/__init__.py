abacdf2b9cee6ee1230a1c7ae3e5875d74bdc698
Kate Wilson <kwilson7@uw.edu>
2019-08-27 13:57:06 -0700

    add dummy risk components for testing

--- src/vivarium_dcpn_hypertension_mgt/components/__init__.py
+++ src/vivarium_dcpn_hypertension_mgt/components/__init__.py
@@ -1,3 +1,4 @@
 from .treatment_profile_model import CVDRiskAttribute, TreatmentProfileModel, TreatmentEffect
 from .treatment_algorithm import Adherence, MeasuredSBP, TreatmentAlgorithm
 from .metrics import SampleHistoryObserver
+from .testing import DummyRisk
